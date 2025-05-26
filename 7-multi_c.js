f8b1313be9dbf9ab28e83f46584a8f9ef1296219
kigehiflorence <florencekigehi@gmail.com>
2025-05-26 16:46:27 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const arg = process.argv[2];
 const count = parseInt(arg);
 
-if (isNaN(count) || count <= 0) {
+if (isNaN(count)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (count > 0) {
   let i = 0;
   while (i < count) {
     console.log('C is fun');
